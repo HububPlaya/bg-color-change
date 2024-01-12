2d652c2c5ffc340e84e974f9f5f6daff58550916
Keelan Moore <135381527+HububPlaya@users.noreply.github.com>
2024-01-12 15:41:44 -0600

    finished

--- bg-color-change/src/App.jsx
+++ bg-color-change/src/App.jsx
@@ -9,19 +9,24 @@ function App() {
 
   return (
     <div className="bg-black w-full h-screen duration-200 " style={{backgroundColor: color}}>
+      <div className="flex justify-center"><h1>Tap the buttons to change the color</h1></div>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
           onClick={() => changeColor("red")}
+          style={{backgroundColor: "red"}}
           className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
-          >test</button>
+          >Red</button>
           <button
           onClick={() => changeColor("blue")}
+          style={{backgroundColor: "blue"}}
           className="outline-none px-4 py-1 rounded-full shadow-lg text-black">
-            test</button>
+            Blue</button>
           <button
           onClick={() => changeColor("yellow")}
-          className="outline-none px-4 py-1 rounded-full shadow-lg text-black">test</button>
+          style={{backgroundColor: "yellow"}}
+          className="outline-none px-4 py-1 rounded-full shadow-lg text-black">
+            Yellow</button>
         </div>
       </div>
     </div>
